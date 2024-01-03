3a4de4928e07cb52e0d37a415aafe62f9a568e73
Nils Nahooy <nils.nahooy@live.com>
2024-01-03 20:07:22 +0100

    update scrollview

--- lua/plugins/scrollview.lua
+++ lua/plugins/scrollview.lua
@@ -8,7 +8,12 @@ return {
         base = "right",
         column = 1,
         signs_on_startup = { "all" },
-        diagnostics_severities = { vim.diagnostic.severity.ERROR },
+        diagnostics_severities = {
+          vim.diagnostic.severity.ERROR,
+          vim.diagnostic.severity.WARN,
+          vim.diagnostic.severity.HINT,
+          vim.diagnostic.severity.INFO,
+        },
       })
     end,
   },
